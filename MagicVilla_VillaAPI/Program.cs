00a16fbbcd07f2900ecc1b51e31657f412f631e0
ABDIEL ANTONIO CORRO <aacorro@outlook.com>
2023-05-09 21:21:43 -0500

    caching profile

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -60,6 +60,11 @@ builder.Services.AddAuthentication(x =>
 
 builder.Services.AddControllers(option =>
 {
+    option.CacheProfiles.Add("Default30",
+    new CacheProfile()
+    {
+        Duration = 30
+    });
     //option.ReturnHttpNotAcceptable=true;  //uncomment for xml
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 
